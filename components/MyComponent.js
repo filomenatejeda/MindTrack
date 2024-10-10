f244bcea4df08621eedca3a910ff98188af58262
filomenatejeda <ftejeda@alumnos.uai.cl>
2024-10-10 18:42:11 -0300

    components

---
+++ components/MyComponent.js
@@ -0,0 +1,12 @@
+// components/MyComponent.js
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <p>Este es un componente desde la carpeta components.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
