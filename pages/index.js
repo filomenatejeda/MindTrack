8d938d1c6b644d2debbcbf2c38e5793e818d4ed1
filomenatejeda <ftejeda@alumnos.uai.cl>
2024-10-10 18:42:06 -0300

    pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+import MyComponent from '../components/MyComponent'; // Importamos el componente
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Next.js</h1>
+      <MyComponent />
+    </div>
+  );
+}
