2ac14cd07a880b0291f11089e68353ab725eb804
tvalw <vmeyer@alumnos.uai.cl>
2024-10-30 19:29:37 -0300

    components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,28 @@
 // components/Navbar.js
 import Link from 'next/link';
+import { useLanguage } from '/context/LanguageContext';
+import { useTranslation } from 'react-i18next'
 
-export default function Navbar() {
+function Navbar() { 
+
+  const {language, changeLanguage } = useLanguage(); // Obtener y cambiar idioma del contexto
+  const { t } = useTranslation(); 
+
   return (
     <nav className="bg-blue-600 p-4 flex justify-between items-center">
       <div className="text-white font-bold text-lg">MindTrack</div>
       <div className="flex space-x-4">
         <Link href="/" className="text-white">
-          Inicio
+        {t('home')} {/* Usa traducción para "Inicio" */}
         </Link>
         <Link href="/log/login" className="text-white">
-          Iniciar Sesión
+        {t('login')} {/* Usa traducción para "Iniciar Sesión" */}
         </Link>
+        <button onClick={() => changeLanguage('es')} className="text-white">Español</button>
+      <button onClick={() => changeLanguage('en')} className="text-white">English</button>
       </div>
     </nav>
   );
 }
 
+export default Navbar;
