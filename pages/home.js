30f5870f0bafed351115b001cc927dd93984b98b
tvalw <vmeyer@alumnos.uai.cl>
2024-10-31 14:54:43 -0300

    pages

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,5 @@
 // pages/home.js
-
-export default function Home() {
-    return (
-      <div className="min-h-screen bg-gray-100 p-8">
-              <script src="https://cdn.tailwindcss.com"></script> {/* Carga Tailwind CSS */}
-        <h1 className="text-3xl font-bold text-blue-900">Bienvenido a Home</h1>
-        <p className="mt-4 text-gray-700">¡Disfruta de tu experiencia en MindTrack!</p>
-      </div>
-    );
-  }
-  
-  
-
+'use client'
 import React, { useState, useEffect } from 'react'; // Asegúrate de importar React y useState
 import MoodSelector from '../components/MoodSelector';
 import Notes from '../components/Notes';
--- pages/layout.js
+++ pages/layout.js
@@ -2,35 +2,25 @@
 import { LanguageProvider } from '../context/LanguageContext';
 import { I18nextProvider } from 'react-i18next';
 import i18next from '../utils/i18n';
-import Navbar from '../components/Navbar'; // Asegúrate de importar tu Navbar
-import Footer from '../components/Footer'; // Asegúrate de importar tu Footer
-// import '../styles/globals.css'; // Importa tus estilos globales (opcional)
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
     <LanguageProvider>
       <I18nextProvider i18n={i18next}>
         <div className="min-h-screen flex flex-col">
           {/* <Navbar /> Barra de navegación */}
-          <main className="flex-grow"> {/* Área principal donde se renderizarán las páginas */}
+          <main className="flex-grow"> {/* Área principal */}
             {children}
           </main>
           {/* <Footer /> Pie de página */}
         </div>
-            </I18nextProvider>
-        </LanguageProvider>
-        </body>
-    </html>
+      </I18nextProvider>
+    </LanguageProvider>
   );
 }
 
 export default RootLayout;
+
 
