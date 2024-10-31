03aee58eaecf2a95e300796daa8e0fb233c23a69
tvalw <vmeyer@alumnos.uai.cl>
2024-10-31 16:25:45 -0300

    pages

--- pages/register/create-user.js
+++ pages/register/create-user.js
@@ -4,9 +4,11 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Navbar from '/components/Navbar';
 import Footer from '/components/Footer';
+import { useTranslation } from 'react-i18next';
 
 export default function CreateUser() {
   const router = useRouter();
+  const { t } = useTranslation();
   
   // Definir estados para cada campo del formulario
   const [firstName, setFirstName] = useState('');
@@ -38,72 +40,75 @@ export default function CreateUser() {
   };
 
   return (
-    <div>
-      <div className="flex flex-col items-center min-h-screen bg-gray-100">
-        <div className="w-full fixed top-0 z-50">
-          <Navbar />
-        </div>
-        <div className="mt-40 w-full flex justify-center">
-          <form onSubmit={handleSubmit} className="p-6 bg-green-100 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold mb-6 text-center text-green-800">Crear Cuenta</h2>
-             <input
-              type="text"
-              placeholder="Nombre de usuario"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Contraseña"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
-              required
-            />
-            <input
-              type="text"
-              placeholder="Nombre"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
-              required
-            />
-            <input
-              type="text"
-              placeholder="Apellido"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
-              required
-            />
-           
-            <input
-              type="email"
-              placeholder="Correo"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
-              required
-            />
-            
-            <button type="submit" className="w-full py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">
-              Crear Cuenta
-            </button>
-            {error && <p className="text-red-600 mt-2 text-center">{error}</p>}
-            <div className="mt-4 text-center">
-              <Link href="/loginQ/login" className="text-green-600">
-                Ya tengo una cuenta
-              </Link>
-            </div>
-          </form>
-        </div>
-        <Footer />
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      {/* Navbar fija en la parte superior */}
+      <div className="w-full fixed top-0 z-50">
+        <Navbar />
       </div>
+  
+      {/* Contenedor central con el formulario ajustado */}
+      <div className="flex-grow mt-28 w-full flex justify-center items-start">
+        <form onSubmit={handleSubmit} className="p-6 bg-green-100 rounded-lg shadow-lg max-w-md w-full">
+          <h2 className="text-2xl font-bold mb-6 text-center text-green-800">
+            {t('register')}
+          </h2>
+          <input
+            type="text"
+            placeholder={t('user')}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+            required
+          />
+          <input
+            type="password"
+            placeholder={t('password')}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+            required
+          />
+          <input
+            type="text"
+            placeholder={t('name')}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+            required
+          />
+          <input
+            type="text"
+            placeholder={t('lastname')}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+            required
+          />
+          <input
+            type="email"
+            placeholder={t('email')}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full mb-4 px-4 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+            required
+          />
+          <button type="submit" className="w-full py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">
+            {t('register')}
+          </button>
+          {error && <p className="text-red-600 mt-2 text-center">{error}</p>}
+          <div className="mt-4 text-center">
+            <Link href="/loginQ/login" className="text-green-600">
+              {t('haveanaccount')}
+            </Link>
+          </div>
+        </form>
+      </div>
+  
+      {/* Footer al final de la página */}
+      <Footer />
     </div>
-  );
-}
+  );  
+}  
 
 
 
