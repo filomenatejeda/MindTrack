f4638386ce59cc0f1635ce883e51892ee0d7015b
filomenatejeda <ftejeda@alumnos.uai.cl>
2024-11-04 01:09:00 -0300

    pages

--- pages/calendar.js
+++ pages/calendar.js
@@ -12,6 +12,7 @@ const CalendarPage = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedEmoji, setSelectedEmoji] = useState(null);
 
+    
     useEffect(() => {
         const today = new Date();
         const todayString = format(today, 'yyyy-MM-dd', { timeZone: 'America/Santiago' });
@@ -43,34 +44,34 @@ const CalendarPage = () => {
 
     const handleEmojiSelect = (index) => {
         const dateString = format(selectedDate, 'yyyy-MM-dd', { timeZone: 'America/Santiago' });
-
+    
         if (selectedEmoji === index) {
             setSelectedEmoji(null);
             removeEmoji(dateString);
-            localStorage.removeItem('selectedEmoji'); // Eliminar emoji guardado
+            localStorage.removeItem('selectedEmoji'); // Remueve emoji de localStorage si se deselecciona
         } else {
             setSelectedEmoji(index);
-            addEmoji(dateString, index);
-            localStorage.setItem('selectedEmoji', JSON.stringify(index)); // Guardar emoji seleccionado
+            addEmoji(dateString, index); // Asegúrate de que esto guarde correctamente el emoji
+            localStorage.setItem('selectedEmoji', JSON.stringify(index)); // Guarda el nuevo emoji
+            console.log('Guardando emoji:', { dateString, index });
         }
     };
+    
 
     const calculateEmojiStatistics = () => {
         const emojiCounts = {};
         const totalEntries = Object.keys(emojiData).length;
 
         Object.values(emojiData).forEach((emoji) => {
-            if (emojiCounts[emoji]) {
-                emojiCounts[emoji]++;
-            } else {
-                emojiCounts[emoji] = 1;
-            }
+            emojiCounts[emoji] = (emojiCounts[emoji] || 0) + 1;
         });
 
-        return Object.entries(emojiCounts).map(([emoji, count]) => ({
-            emoji,
-            percentage: ((count / totalEntries) * 100).toFixed(2),
-        })).filter(stat => stat.percentage !== "100.00");
+        return Object.entries(emojiCounts)
+            .map(([emoji, count]) => ({
+                emoji,
+                percentage: ((count / totalEntries) * 100).toFixed(2),
+            }))
+            .filter(stat => stat.percentage !== "100.00");
     };
 
     const emojiStatistics = calculateEmojiStatistics();
@@ -80,8 +81,9 @@ const CalendarPage = () => {
             <NavbarH />
             <div className="flex flex-row items-start justify-center flex-grow p-6">
                 <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
-                    <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">Calendario de Emojis</h1>
+                    <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">Calendario de Emociones</h1>
                     <p className="text-center text-gray-600 mb-4">Monitorea tus emociones día a día</p>
+                    
                     <Calendar
                         onChange={handleDateChange}
                         value={selectedDate}
@@ -91,12 +93,8 @@ const CalendarPage = () => {
                             const isToday = dateString === format(new Date(), 'yyyy-MM-dd', { timeZone: 'America/Santiago' });
                             const isFutureDate = date > new Date();
 
-                            if (isToday) {
-                                return 'bg-blue-300';
-                            } 
-                            if (isFutureDate) {
-                                return 'bg-gray-300';
-                            }
+                            if (isToday) return 'bg-blue-300';
+                            if (isFutureDate) return 'bg-gray-300';
                             return emojiData[dateString] ? 'bg-yellow-200' : '';
                         }}
                     />
@@ -106,9 +104,10 @@ const CalendarPage = () => {
                             <EmojiIcons selectedEmoji={selectedEmoji} onEmojiSelect={handleEmojiSelect} />
                         )}
                     </div>
+
                     <div className="text-center">
                         {selectedEmoji !== null && (
-                            <p className="text-gray-600">Emoji seleccionado: {emojis[selectedEmoji].expression}</p>
+                            <p className="text-gray-600">¿Cómo te sientes?: {emojis[selectedEmoji].expression}</p>
                         )}
                     </div>
                 </div>
@@ -117,45 +116,37 @@ const CalendarPage = () => {
                 <div className="bg-white shadow-lg rounded-lg p-8 max-w-xs w-full ml-4">
                     <h2 className="text-2xl font-bold text-center text-gray-800 mb-4">Estadísticas Emociones</h2>
                     {emojiStatistics.length > 0 ? (
-    emojiStatistics.map(({ emoji, percentage }) => (
-        <div key={emoji} className="mb-4">
-            <div className="flex justify-between items-center mb-2">
-                <span className="flex items-center">
-                    <span className="mr-2" role="img" aria-label="emoji">
-                        {/* Asegúrate de que emojis[emoji] esté definido */}
-                        {emojis[emoji] && (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="30" height="30"
-                                viewBox="0 0 100 100"
-                            >
-                                <circle cx="50" cy="50" r="40" fill={emojis[emoji].color} stroke="black" strokeWidth="2" />
-                                <circle cx="35" cy="40" r="5" fill="black" />
-                                <circle cx="65" cy="40" r="5" fill="black" />
-                                {emoji === 0 && <path d="M30 65 Q50 90 70 65" fill="none" stroke="black" strokeWidth="2" />} {/* Muy feliz */}
-                                {emoji === 1 && <path d="M30 60 Q50 80 70 60" fill="none" stroke="black" strokeWidth="2" />} {/* Feliz */}
-                                {emoji === 2 && <line x1="30" y1="65" x2="70" y2="65" stroke="black" strokeWidth="2" />} {/* Neutral */}
-                                {emoji === 3 && <path d="M30 65 Q50 50 70 65" fill="none" stroke="black" strokeWidth="2" />} {/* Llorando */}
-                                {emoji === 4 && <path d="M70 65 Q50 50 30 65" fill="none" stroke="black" strokeWidth="2" />} {/* Triste */}
-                            </svg>
-                        )}
-                    </span>
-                    {emojis[emoji]?.expression} {/* Uso del operador opcional para evitar errores */}
-                </span>
-                <span>{percentage}%</span>
-            </div>
-            <div className="bg-gray-300 h-2 rounded">
-                <div
-                    className="bg-blue-600 h-full rounded"
-                    style={{ width: `${percentage}%` }}
-                />
-            </div>
-        </div>
-    ))
-) : (
-    <p className="text-gray-600 text-center">No hay datos suficientes para mostrar.</p>
-)}
-
+                        emojiStatistics.map(({ emoji, percentage }) => (
+                            <div key={emoji} className="mb-4">
+                                <div className="flex justify-between items-center mb-2">
+                                    <span className="flex items-center">
+                                        <span className="mr-2" role="img" aria-label="emoji">
+                                            {/* Asegúrate de que emojis[emoji] esté definido */}
+                                            {emojis[emoji] && (
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 100 100">
+                                                    <circle cx="50" cy="50" r="40" fill={emojis[emoji].color} stroke="black" strokeWidth="2" />
+                                                    <circle cx="35" cy="40" r="5" fill="black" />
+                                                    <circle cx="65" cy="40" r="5" fill="black" />
+                                                    {emoji === 0 && <path d="M30 65 Q50 90 70 65" fill="none" stroke="black" strokeWidth="2" />} {/* Muy feliz */}
+                                                    {emoji === 1 && <path d="M30 60 Q50 80 70 60" fill="none" stroke="black" strokeWidth="2" />} {/* Feliz */}
+                                                    {emoji === 2 && <line x1="30" y1="65" x2="70" y2="65" stroke="black" strokeWidth="2" />} {/* Neutral */}
+                                                    {emoji === 3 && <path d="M30 65 Q50 50 70 65" fill="none" stroke="black" strokeWidth="2" />} {/* Llorando */}
+                                                    {emoji === 4 && <path d="M70 65 Q50 50 30 65" fill="none" stroke="black" strokeWidth="2" />} {/* Triste */}
+                                                </svg>
+                                            )}
+                                        </span>
+                                        {emojis[emoji]?.expression} {/* Uso del operador opcional para evitar errores */}
+                                    </span>
+                                    <span>{percentage}%</span>
+                                </div>
+                                <div className="bg-gray-300 h-2 rounded">
+                                    <div className="bg-blue-600 h-full rounded" style={{ width: `${percentage}%` }} />
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-gray-600 text-center">No hay datos suficientes para mostrar.</p>
+                    )}
                 </div>
             </div>
             <Footer />
